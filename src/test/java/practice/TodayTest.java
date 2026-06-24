1a60832935dd6465d69e43ddffb16e2e98e13149
Jagu <jagathgowfa1997@gmail.com>
2026-06-24 20:15:54 +0530

    asdefghj

--- src/test/java/practice/TodayTest.java
+++ src/test/java/practice/TodayTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class todaytest {
+public class TodayTest {
 
 	@Test
 	public void openvbrowser() throws InterruptedException{
